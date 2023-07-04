9387a26eb65cd1cab70e0108536f8e1f8dc1c352
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-07-04 17:22:00 +0500

    on resize events

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
+import { useEffect } from "react";
 import "./resizable.css";
 
 interface ResizableProps {
@@ -9,8 +10,20 @@ interface ResizableProps {
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
 
+  useEffect(() => {
+    const listener = () => {
+      console.log(window.innerWidth, window.innerHeight);
+    };
+    window.addEventListener("resize", listener);
+
+    return () => {
+      window.removeEventListener("resize", listener);
+    };
+  }, []);
+
   if (direction === "horizontal") {
     resizableProps = {
+      className: "resize-horizontal",
       minConstraints: [window.innerWidth * 0.2, Infinity],
       maxConstraints: [window.innerWidth * 0.75, Infinity],
       height: Infinity,
