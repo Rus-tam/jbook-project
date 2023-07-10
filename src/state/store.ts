b01b0be561c3efd0baf08694b177ab274d3e998b
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-07-10 10:59:46 +0500

    back to react

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,16 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
+// import { ActionTypes } from "./action-types";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+// store.dispatch({
+//   type: ActionTypes.INSERT_CELL_BEFORE,
+//   payload: {
+//     id: null,
+//     type: "code",
+//   },
+// });
+//
+// console.log(store.getState());
